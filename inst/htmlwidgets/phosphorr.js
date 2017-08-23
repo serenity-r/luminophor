ee2e890b4b1974a415e53c94b24f3e46e033b549
M. Drew LaMar <mdlama@wm.edu>
2017-08-23 09:01:37 -0400

    Styling + getBox method in HTMLWidget

--- inst/htmlwidgets/phosphorr.js
+++ inst/htmlwidgets/phosphorr.js
@@ -6,37 +6,42 @@ HTMLWidgets.widget({
 
   factory: function(el, width, height) {
 
-    var main = null;
+    var box = null;
 
     return {
 
       renderValue: function(x) {
 
-        if (main === null) {
+        if (box === null) {
           // Create BoxPanel
-  	      main = new phosphorjs.BoxPanel({ direction: 'left-to-right', spacing: 0 });
-  	      main.id = 'main';
+  	      box = new phosphorjs.BoxPanel({ direction: 'left-to-right', spacing: 0 });
 
   	      // Create DockPanel
   	      var dock = new phosphorjs.DockPanel();
-  	      dock.id = 'dock';
 
   	      // Create ContentWidget (right now, uses predefined function)
   	      var r1 = new phosphorjs.ContentWidget('Red');
+  	      var r2 = new phosphorjs.ContentWidget('Blue');
 
   	      // Add widgets in appropriate places - this can be done in any order
   	      dock.addWidget(r1);
-  	      main.addWidget(dock);
+  	      dock.addWidget(r2);
+  	      box.addWidget(dock);
 
-  	      // Attach BoxPanel to document.body (could be any element)
-  	      phosphorjs.Widget.attach(main, el);
+  	      // Attach BoxPanel to el
+  	      phosphorjs.Widget.attach(box, el);
         }
       },
 
       resize: function(width, height) {
-        main.update();
-      }
+        box.update();
+      },
 
+      // Give access to box if anyone needs it on the outside
+      // Using in console:  HTMLWidgets.find(".phosphorr").getBox()
+      getBox: function() {
+        return box;
+      }
     };
   }
 });
